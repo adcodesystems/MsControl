f65182019a8030a3112c9fbd19dd4f74c393b8d9
Alonso <aguerovilchez@gmail.com>
2022-09-26 23:26:11 -0500

    Sexta versión - Login y Prelogin

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonaNaturalFormComponent } from './components/persona-natural-form/persona-natural-form.component';
 import { PreloginComponent } from './components/prelogin/prelogin.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonaNaturalFormComponent,
-    PreloginComponent
+    PreloginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
